7c299cec99572a2755785e9f81b8535d0e7646d5
Abhinav <aa5561@columbia.edu>
2026-09-21 00:18:52 -0400

    connect supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <h1>Hello World</h1>;
+import { supabase } from '@/lib/supabase'
+
+export default async function Home() {
+  const { data: messages } = await supabase.from('messages').select()
+
+  return (
+    <ul>
+      {messages?.map((m) => (
+        <li key={m.id}>{m.content}</li>
+      ))}
+    </ul>
+  )
 }
